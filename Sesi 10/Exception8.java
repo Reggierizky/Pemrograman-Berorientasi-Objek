1a6c2f3ac3e8a60b69787d13dd7437d518857c30
Reggierizky <reggie.rizky_ti24@nusaputra.ac.id>
2026-07-06 16:37:33 +0700

    Add files via upload
    PBO

---
+++ Sesi 10/Exception8.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+public class Exception8 {
+    public void methodA() {
+        System.out.println("Method A");
+    }
+
+    public void methodB() throws IOException {
+        System.out.println(20 / 0);
+        System.out.println("Method B");
+    }
+
+    public static void main(String[] args) {
+        Exception8 o = new Exception8();
+        o.methodA();
+        try {
+            o.methodB();
+        } catch (Exception e) {
+            System.out.println("Error di Method B");
+        } finally {
+            System.out.println("Ini selalu dicetak");
+        }
+    }
+}
